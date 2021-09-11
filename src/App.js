5540d88cd3a7ee528ada117d6905973dc43b33c9
EgamiDark <matias.ignacio.sanmartins@gmail.com>
2021-09-10 21:27:44 -0300

    Dashboard+login

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Login from './templates/SingIn'
+import DashBoard from './templates/dashboard/Dashboard'
+
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
